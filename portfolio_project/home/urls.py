94d9d476d84269603518a59de034c189567acc9a
Pedro <pedromiguelnevesdemeu@gmail.com>
2021-06-03 14:40:02 -0300

    Feat: Menu de Navegação

--- portfolio_project/home/urls.py
+++ portfolio_project/home/urls.py
@@ -5,4 +5,4 @@ from django.conf.urls.static import static
 from .views import render_home
 urlpatterns = [
     path('', render_home)
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

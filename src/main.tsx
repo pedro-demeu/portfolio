cc72b2343afd190aa10e5c768bb6a85ac9808043
pdemeu <pdemeu.pessoal@gmail.com>
2025-02-05 19:45:46 -0300

    [Feat] - style scroll

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import "./styles.css"
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
